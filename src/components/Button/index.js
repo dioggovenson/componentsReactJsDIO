5e3f137c3f66796c4e4591bb600281cbf4647169
Dioggo Venson <dioggovenson@gmail.com>
2022-02-14 16:19:58 -0300

    First Commit

---
+++ src/components/Button/index.js
@@ -0,0 +1,8 @@
+const Button = ({ children, className, onClick }) => {
+    return (
+        <button type="button"
+            className={className}
+            onClick={onClick}>{children}</button>
+    )
+}
+export default Button
